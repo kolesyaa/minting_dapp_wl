0d8fbf114f945cfeb4efdcb6ec50b845b5cb2b32
kolesyaa <56188793+kolesyaa@users.noreply.github.com>
2022-07-17 21:07:29 +0400

    Add files via upload

---
+++ dapp.config.js
@@ -0,0 +1,11 @@
+const config = {
+  title: 'AI Okay Bears Mint',
+  contractAddress: '0xED6346e8Bf42ef9E624646ccE19964C26E9517BA',
+  maxMintAmount: 5,
+  presaleMaxMintAmount: 5,
+  price: 0.0098
+}
+
+
+
+export { config }
